00c75ff1feabdb24bbdd4cc219f6effb6956e28a
Timunas <joao.suzana@gmail.com>
2018-03-17 18:44:11 +0000

    Add javadoc to CompetitorResult enum

--- src/main/java/com/timunas/core/CompetitorResult.java
+++ src/main/java/com/timunas/core/CompetitorResult.java
@@ -1,5 +1,11 @@
 package com.timunas.core;
 
+/**
+ * This enum represents the possible {@link Competitor} results when result is not time.
+ *
+ * @author Timunas
+ * @since  1.0
+ */ 
 public enum CompetitorResult {
     DNS, // Did Not Start
     DNF, // Did Not Finish
